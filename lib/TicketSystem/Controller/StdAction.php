e21c3973a816f304ad8da16e1de99c66227788b8
Eric Rasmussen <ericrasmussen1@gmail.com>
2010-03-18 21:29:24 +0000

    condensed all navigation into one unit, modified styling on the unl skin

--- lib/TicketSystem/Controller/StdAction.php
+++ lib/TicketSystem/Controller/StdAction.php
@@ -11,59 +11,71 @@ class TicketSystem_Controller_StdAction extends TicketSystem_Controller_EmptyAct
         $isAuthenticated = Zend_Auth::getInstance()->hasIdentity();
         
         $metanav = array();
-        if ($isAuthenticated) {
-            $metanav[] = array(
-                'label' => 'Logout',
-                'controller' => 'auth',
-                'action' => 'logout'
+
+        if ($isAuthenticated) { 
+        	$metanav[] = array(
+                'label' => 'View Tickets ',
+                'controller' => 'index',
+                'action' => 'index',
+        		'class' => 'viewtickets',
+                'active' => ($this->_isActiveNav('index', 'index') || $this->_isActiveNav('view', 'ticket') ||
+                             $this->_isActiveNav('index', 'report')|| $this->_isActiveNav('view', 'report'))
             );
-            
+            if ($this->_isAclAllowed((string)Zend_Auth::getInstance()->getIdentity()->level, 'ticket', 'create')) {
+                $metanav[] = array(
+                    'label' => 'New Ticket ',
+                    'controller' => 'ticket',
+                    'action' => 'new',
+        			'class' => 'newticket',
+                    'active' => $this->_isActiveNav('new', 'ticket')
+                );
+            }
+            $metanav[] = array(
+                'label' => 'Search',
+                'controller' => 'ticket',
+                'action' => 'search',
+        		'class' => 'searchtickets',
+                'active' => ($this->_isActiveNav('search', 'ticket') || $this->_isActiveNav('results', 'ticket'))
+            );       
             $metanav[] = array(
                 'label' => 'Configuration',
                 'controller' => 'config',
-                'action' => 'profile'
+                'action' => 'profile',
+        		'class' => 'configuration',
+                'active' => $this->_isActiveNav('profile', 'config')
+            );
+            $metanav[] = array(
+	            'label' => 'Help',
+	            'controller' => 'help',
+	            'action' => 'index',
+        		'class' => 'help',
+                'active' => $this->_isActiveNav('','help')
+	        );
+            $metanav[] = array(
+                'label' => 'Logout',
+                'controller' => 'auth',
+                'action' => 'logout',
+        		'class' => 'logout',
+                'active' => $this->_isActiveNav('logout', 'auth')
             );
         } else {
             $metanav[] = array(
                 'label' => 'Login',
                 'controller' => 'auth',
-                'action' => 'index'
-            );
-        }
-        
-        $metanav[] = array(
-            'label' => 'Help',
-            'controller' => 'help',
-            'action' => 'index'
-        );
-        $this->view->metanav = $metanav;
-        
-        if ($isAuthenticated) {
-            $nav = array(array(
-                'label' => 'View Tickets ',
-                'controller' => 'index',
                 'action' => 'index',
-                'active' => ($this->_isActiveNav('index', 'index') || $this->_isActiveNav('view', 'ticket') ||
-                             $this->_isActiveNav('index', 'report')|| $this->_isActiveNav('view', 'report'))
-            ));
-            if ($this->_isAclAllowed((string)Zend_Auth::getInstance()->getIdentity()->level, 'ticket', 'create')) {
-                $nav[] = array(
-                    'label' => 'New Ticket ',
-                    'controller' => 'ticket',
-                    'action' => 'new',
-                    'active' => $this->_isActiveNav('new', 'ticket')
-                );
-            }
-            $nav[] = array(
-                'label' => 'Search',
-                'controller' => 'ticket',
-                'action' => 'search',
-                'active' => ($this->_isActiveNav('search', 'ticket') || $this->_isActiveNav('results', 'ticket'))
+        		'class' => 'login',
+                'active' => $this->_isActiveNav('login', 'auth')
             );
-        } else {
-            $nav = array();
+            $metanav[] = array(
+	            'label' => 'Help',
+	            'controller' => 'help',
+	            'action' => 'index',
+        		'class' => 'help',
+                'active' => $this->_isActiveNav('help')
+	        );
         }
-        $this->view->mainnav = $nav;
+        
+        $this->view->metanav = $metanav;
         
         parent::postDispatch();
     }
